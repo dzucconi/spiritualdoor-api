329ff35d502cc9d0e1b95ca22b986c14da93e864
dzucconi <mail@damonzucconi.com>
2016-08-08 13:44:21 +0200

    Remove `updated_at` from JSON response

--- models/heading.rb
+++ models/heading.rb
@@ -7,7 +7,7 @@ class Heading
   field :referer, type: String
   field :rate, type: Integer
 
-  index(created_at: -1)
+  index created_at: -1
 
   validates_presence_of :value
   validates_numericality_of :value,
@@ -15,9 +15,8 @@ class Heading
                             less_than_or_equal_to: 360.0
 
   def as_json(options = {})
-    attrs = super(options)
+    attrs = super options
     attrs['id'] = attrs['_id'].to_s
-    attrs.delete('_id')
-    attrs
+    attrs.except! '_id', 'updated_at'
   end
 end
